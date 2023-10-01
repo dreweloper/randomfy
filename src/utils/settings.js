d87299dfa57606580849f3c711208682620553b1
Andrés León <137108028+dreweloper@users.noreply.github.com>
2023-10-01 16:23:55 +0200

    Handling user error in custom hook.

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,7 +1,7 @@
 // AUTH
+export const BASE_URL = 'http://localhost:3000/api';
 export const REDIRECT_URI = 'http://localhost:5173/login';
 export const SCOPE = 'user-read-private user-read-email playlist-read-private playlist-read-collaborative user-library-read user-library-modify';
-export const URL_BASE = 'http://localhost:3000/api';
 
 // COOKIES
 export const ACCESS_TOKEN_KEY = 'access_token';
