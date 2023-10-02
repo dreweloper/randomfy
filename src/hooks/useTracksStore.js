2c5dca46d85c90c5f4826b8e483f987a019da3ed
Andrés León <137108028+dreweloper@users.noreply.github.com>
2023-10-02 13:18:47 +0200

    Func 'checkIfUserFollowPlaylist' done.

--- src/hooks/useTracksStore.js
+++ src/hooks/useTracksStore.js
@@ -1,5 +1,5 @@
 import { useCookies } from 'react-cookie';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { generateRandomNumber } from '../helpers';
 import { addTrack, setTrackError, startTrackLoading } from '../store/slices';
 import { ACCESS_TOKEN_KEY, SPOTIFY_BASE_URL, USER_ID } from '../utils';
@@ -7,6 +7,8 @@ import { ACCESS_TOKEN_KEY, SPOTIFY_BASE_URL, USER_ID } from '../utils';
 export const useTracksStore = () => {
 
     // REACT-REDUX HOOKS
+    const { user } = useSelector(state => state.user);
+
     const dispatch = useDispatch();
 
     // REACT-COOKIE HOOK
@@ -40,11 +42,11 @@ export const useTracksStore = () => {
 
     };
 
-    const getRandomUserPlaylistId = async (total) => {
+    const getRandomUserPlaylistId = async (totalPlaylists) => {
 
         try {
 
-            const randomOffset = generateRandomNumber(total);
+            const randomOffset = generateRandomNumber(totalPlaylists);
 
             const response = await fetch(`${SPOTIFY_BASE_URL}/v1/users/${USER_ID}/playlists?limit=1&offset=${randomOffset}`, fetchOptions);
 
@@ -68,6 +70,33 @@ export const useTracksStore = () => {
 
     };
 
+    const checkIfUserFollowPlaylist = async (playlistId, userId) => {
+
+        try {
+
+            const response = await fetch(`${SPOTIFY_BASE_URL}/v1/playlists/${playlistId}/followers/contains?ids=${userId}`, fetchOptions);
+
+            if (!response.ok) {
+
+                throw new Error('Failed to check if user follows the playlist');
+
+            } else {
+
+                const [data] = await response.json();
+
+                return data;
+
+            };
+
+        } catch (error) {
+
+            throw error;
+
+        };
+
+    };
+
+
     const init = async () => {
 
         try {
@@ -77,6 +106,8 @@ export const useTracksStore = () => {
             const total = await getUserTotalPlaylists();
 
             const playlist_id = await getRandomUserPlaylistId(total);
+
+            const isFollowed = await checkIfUserFollowPlaylist(playlist_id, user.id);
 
         } catch (error) {
 
