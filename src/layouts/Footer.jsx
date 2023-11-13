63f35b9f2ffbbc1aa92db7cbee76ed1dc3f3993f
Andrés León <137108028+dreweloper@users.noreply.github.com>
2023-11-13 21:19:54 +0100

    Footer basic mobile styles done.

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from '../sass/layouts/_Footer.module.scss';
 
 export const Footer = () => {
@@ -5,7 +6,35 @@ export const Footer = () => {
 
     return (
 
-        <footer className={styles.footer}>Footer</footer>
+        <footer className={styles.footer}>
+
+            <nav className={styles.nav}>
+
+                <Link
+                    className={styles.link}
+                    to='https://github.com/dreweloper'
+                    target='_blank'
+                >
+
+                    <i className={`${styles.logo} devicon-github-original`}></i>
+
+                </Link>
+
+                <Link
+                    className={styles.link}
+                    to='https://www.linkedin.com/in/andres-leon-developer'
+                    target='_blank'
+                >
+
+                    <i className={`${styles.logo} devicon-linkedin-plain`}></i>
+
+                </Link>
+
+            </nav>
+
+            <p className={styles.text}>© 2023. A project by <strong>Andrés León</strong></p>
+
+        </footer>
 
     );
 
